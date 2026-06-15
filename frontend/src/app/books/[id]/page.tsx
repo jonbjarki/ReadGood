a22a656e2653fbf9cdc94e7963754a6b02703c38
jonbjarki <jonbjarki02@gmail.com>
2026-06-15 22:07:27 +0000

    improved book details page design

--- frontend/src/app/books/[id]/page.tsx
+++ frontend/src/app/books/[id]/page.tsx
@@ -4,6 +4,7 @@ import DOMPurify from 'dompurify';
 import parse from 'html-react-parser';
 import { notFound } from "next/navigation";
 import { JSDOM } from "jsdom"
+import { Button } from "@/components/ui/button";
 
 async function fetchBookAction(bookId: string): Promise<BookItem> {
     const res = await fetch(process.env.API_URL + "books/" + bookId, { next: { revalidate: 300 } }); // cache for 5 minutes
@@ -28,33 +29,45 @@ export default async function BookPage({ params }: { params: Promise<{ id: strin
     // Sanitizes and parses description as there are often simple HTML tags included "<p> <b> <i> etc."
     const window = new JSDOM('').window;
     const purify = DOMPurify(window);
-    const sanitizedDescription = purify.sanitize(book.description ?? "")
+    console.log("unsanitized:", "".concat(book.description!));
+    const sanitizedDescription = purify.sanitize(book.description ?? "", {
+        ALLOWED_TAGS: ["<br>"]
+    })
     const description = parse(sanitizedDescription);
 
     return (
         <main className="px-4 py-8 max-w-3xl mx-auto">
-            <div className="flex flex-col md:flex-row gap-8">
+            <div className="flex flex-col md:flex-row gap-8 mb-8 items-center">
                 <div className="shrink w-full md:w-48">
                     <div className="relative w-full h-0 pb-[150%] md:pb-[150%]">
                         <CoverImageWithFallback url={book.coverImageUrl} title={book.title} />
                     </div>
                 </div>
 
-                {/* main content */}
-                <div className="flex-1">
-                    <h1 className="text-2xl font-bold">{book.title}</h1>    
-                    <div className="mt-4 space-y-1 text-sm text-gray-600">
-                        {book.authorName && (
-                            <p>
-                                By {book.authorName} {book.firstPublishedYear && ("· " + book.firstPublishedYear)}
-                            </p>
-                        )}
+                <div className="flex flex-col shrink-2 basis-0 grow items-start gap-4">
+                    <h1 className="text-2xl font-bold">{book.title}</h1>
+                    <div className="space-y-1 text-lg">
+                        <p>{authorAndYear(book.authorName, book.firstPublishedYear)}</p>
                     </div>
-                    <div className="text-balance whitespace-pre-wrap leading-relaxed mt-4 max-h-40 overflow-y-scroll">
-                        {description}
-                    </div>
+                    <Button>Add to bookshelf</Button>
                 </div>
             </div>
+            <p className="leading-7 text-pretty">
+                {description}
+            </p>
         </main>
     );
+}
+
+function authorAndYear(authorName: string | null, year?: string | null) {
+    if (authorName && year) {
+        return `${authorName} · ${year}`
+    }
+    else if (authorName && !year) {
+        return authorName
+    }
+    else if (!authorName && year) {
+        return year
+    }
+
 }
